37dd394cc4f6cf3c199fee5d378c6fc307e9782f
Boris Shkarov <b.shkarov@gmail.com>
2020-11-22 01:07:09 +0300

    Камень судьбы.Доработка.

---
+++ src/classes/Task.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace TaskForce\classes;
+
+class Task
+{
+    const STATUS_NEW = 'new';
+    const STATUS_CANCELED = 'canceled';
+    const STATUS_WORK = 'work';
+    const STATUS_DONE = 'done';
+    const STATUS_FAILED = 'failed';
+
+    const ACTION_CANCEL = 'cancel';
+    const ACTION_COMPLETE = 'complete';
+    const ACTION_RESPOND = 'respond';
+    const ACTION_REFUSE = 'refuse';
+
+    public $status;
+
+    private $id_customer;
+    private $id_builder;
+
+
+    public function __construct($id_customer, $id_builder)
+    {
+        $this->id_customer = $id_customer;
+        $this->id_builder = $id_builder;
+    }
+
+//класс имеет методы для возврата «карты» статусов и действий.
+
+public function getStatusesMap()
+{
+    return [
+        self::STATUS_NEW => 'Новое',
+        self::STATUS_CANCELED => 'Отменено',
+        self::STATUS_WORK => 'В работе',
+        self::STATUS_DONE => 'Выполнено',
+        self::STATUS_FAILED => 'Провалено'
+    ];
+}
+
+public function getActionsMap()
+{
+    return [
+        self::ACTION_CANCEL => 'Отмененить',
+        self::ACTION_COMPLETE => 'Выполнено',
+        self::ACTION_RESPOND => 'Откликнуться',
+        self::ACTION_REFUSE => 'Отказаться'
+    ];
+}
+
+//класс имеет метод для получения статуса, в которой он перейдёт после выполнения указанного действия
+
+    public function getNextStatus($action)
+    {
+        if ($action === self::ACTION_CANCEL) {
+            return self::STATUS_CANCELED;
+        }
+        if ($action === self::ACTION_COMPLETE) {
+            return self::STATUS_DONE;
+        }
+        if ($action === self::ACTION_RESPOND) {
+            return self::STATUS_WORK;
+        }
+        if ($action === self::ACTION_REFUSE) {
+            return self::STATUS_FAILED;
+        }
+        return null;
+    }
+
+//класс имеет метод для получения доступных действий для указанного статуса
+
+    public function getAvaliableAction($status, $id_user)
+    {
+        if ($status === self::STATUS_NEW) {
+            if ($id_user === $this->id_customer) {
+                return self::ACTION_CANCEL;
+            }
+            if ($id_user === $this->id_builder) {
+                return self::ACTION_RESPOND;
+            }
+            return null;
+        }
+        if ($status === self::STATUS_WORK) {
+            if ($id_user === $this->id_customer) {
+                return self::ACTION_COMPLETE;
+            }
+            if ($id_user === $this->id_builder) {
+                return self::ACTION_REFUSE;
+            }
+            return null;
+        }
+        return null;
+    }
+}
---
+++ test.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use TaskForce\classes\Task;
+
+$Task1 = new Task(1,2);
+
+assert($Task1->getStatusesMap() == ['new' => 'Новое',
+                                    'canceled' => 'Отменено',
+                                    'work' => 'В работе',
+                                    'done' => 'Выполнено',
+                                    'failed' => 'Провалено'
+                                   ],
+                                   'Warning: StatusesMap');
+
+assert($Task1->getActionsMap() == ['cancel' => 'Отмененить',
+                                   'complete' => 'Выполнено',
+                                   'respond' => 'Откликнуться',
+                                   'refuse' => 'Отказаться'
+                                  ],
+                                  'Warning: ActionsMap');
+
+$Task1->status = Task::STATUS_NEW;
+
+assert($Task1->getNextStatus('cancel') == Task::STATUS_CANCELED, 'Warning: cancel action');
+assert($Task1->getNextStatus('respond') == Task::STATUS_WORK, 'Warning: respond action');
+
+$Task1->status = Task::STATUS_WORK;
+
+assert($Task1->getNextStatus('complete') == Task::STATUS_DONE, 'Warning: complete action');
+assert($Task1->getNextStatus('refuse') == Task::STATUS_FAILED, 'Warning: refuse action');
+
+assert($Task1->getAvaliableAction('new', 1) == Task::ACTION_CANCEL, 'Warning: cancel action');
+assert($Task1->getAvaliableAction('new', 2) == Task::ACTION_RESPOND, 'Warning: respond action');
+
+assert($Task1->getAvaliableAction('work', 1) == Task::ACTION_COMPLETE, 'Warning: complete action');
+assert($Task1->getAvaliableAction('work', 2) == Task::ACTION_REFUSE, 'Warning: refuse action');
